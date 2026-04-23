b0204e427935c6d4d2df0e0d51c2234946315c30
Nike <198646766+BlakKMango@users.noreply.github.com>
2026-04-23 22:33:03 +0100

    Implement autocomplete suggestions for the search bar

--- src/display.js
+++ src/display.js
@@ -1,3 +1,4 @@
+// ------------------------------------CREATE ELEMENT CLASSES----------------------------------//
 class Element {
     constructor(tag = "div", classNames = [], attributes = {}, eventListenerFns = {}) {
         this.el = document.createElement(tag)
@@ -14,4 +15,48 @@ class Element {
             this.el.addEventListener(key, value)
         })
     }
+}
+
+class Text extends Element {
+  constructor(tag, textContent = "", classNames = [], attributes = {}, eventListenerFns = {}) {
+    super(tag, classNames, attributes, eventListenerFns);
+    this.el.textContent = textContent;
+  }
+}
+
+class SearchSuggestions extends Element {
+    constructor(eventListenerFns = {}, data) {
+        super("ul", ["suggestion-list"], {}, {})
+
+        if (data.length > 0) {
+            data.forEach((d) => {
+                const listItem = new Element("li", ["suggestion"], {}, eventListenerFns)
+                listItem.el.textContent = `${d.city}, ${d.state}`
+                this.el.appendChild(listItem.el)
+            })
+        } else {
+            const listItem = new Element("li", ["suggestion"], {}, eventListenerFns)
+            listItem.el.textContent = "Type at least three letters"
+            this.el.appendChild(listItem.el)
+        }
+
+    }
+}
+
+// ------------------------------------SEARCH BAR---------------------------------------------//
+function displaySuggestions(inputEl, data) {
+    const existingList = document.querySelector(".suggestion-list")
+    if (existingList) {
+        existingList.remove()
+    } 
+    const suggestions = new SearchSuggestions({
+        click: (e) => {
+            inputEl.value = e.target.textContent
+        }}, data)
+    document.querySelector("#search-form").appendChild(suggestions.el)
+}
+
+
+export {
+    displaySuggestions,
 }
--- src/search.js
+++ src/search.js
@@ -1,3 +1,5 @@
+import { displaySuggestions } from "./display.js";
+
 let keyPressTimer;
 
 function initSearch() {
@@ -10,18 +12,19 @@ function initSearch() {
 
 function suggestionHandler(e) {
     const query = e.target.value
+    const inputEl = e.target
     clearTimeout(keyPressTimer)
     
     keyPressTimer = setTimeout(() => {
-        fetchCitySuggestions(query)
-    }, 200);
+        fetchCitySuggestions(inputEl, query)
+    }, 100);
 }
 
-async function fetchCitySuggestions(query) {
+async function fetchCitySuggestions(inputEl, query) {
     try{
         const response = await fetch(`https://api.geoapify.com/v1/geocode/autocomplete?text=${query}&type=city&limit=5&lang=en&format=json&apiKey=${process.env.GEOAPIFY_API_KEY}`)
         const data = await response.json()
-        return data
+        displaySuggestions(inputEl, data.results)
     } catch(e) {
         console.error(e)
     }
