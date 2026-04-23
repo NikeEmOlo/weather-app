c3b4a1fe3beb1195b91607e8a837e82bdb4dfbdf
Nike <198646766+BlakKMango@users.noreply.github.com>
2026-04-23 12:31:38 +0100

    Add a submit button to search form

---
+++ src/search.js
@@ -0,0 +1,12 @@
+function initSearch() {
+    //add event listeners
+    //event listeners run a handler
+}
+
+function searchHandler() {
+    //async api call to the autocomplete service
+}
+
+function searchValidation() {
+    //
+}
